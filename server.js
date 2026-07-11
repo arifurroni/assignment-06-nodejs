279bb0fcff2dbeaccdeebf3c89dcc0affc620c04
Md. Arifur Rahman <arifurroni@gmail.com>
2026-07-12 01:13:19 +0600

    Add Contact Route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const server = http.createServer((req, res) => {
     } else if (req.url === "/about") {
         res.writeHead(200, { 'content-type' : 'text/plain'});
         res.end(`This is About Page`);
+    } else if (req.url === "/contact") {
+        res.writeHead(200, { 'content-type' : 'text/plain'});
+        res.end(`This is Contact Page`);
     } else {
         res.writeHead(404, { 'content-type' : 'text/plain'});
         res.end(`404, Page Not Found`);
