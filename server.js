d1ef79e7e9664239192eeb4090afbcc6101197c5
Md. Arifur Rahman <arifurroni@gmail.com>
2026-07-12 01:11:04 +0600

    Add About Route

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'content-type' : 'text/plain'});
         res.end(`This is Home Page`);
+    } else if (req.url === "/about") {
+        res.writeHead(200, { 'content-type' : 'text/plain'});
+        res.end(`This is About Page`);
     } else {
         res.writeHead(404, { 'content-type' : 'text/plain'});
-        res.end(`404 Not Found`);
+        res.end(`404, Page Not Found`);
     }
 });
 
